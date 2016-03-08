2f24e90ad2066d3b12d9ee462d0ca196dd18a1ae
Alex Sales <electricalsafety@terra.com.br>
2016-03-07 22:03:25 -0300

    Inclusão de estilo na página inicial do projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -3,8 +3,10 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Pagina inicial do projeto</title>
+	<link rel= "stylesheet" href="bootstrap/min.css"/>
 </head>
 <body>
-Pagina inicial do projeto	
+<h1>Pagina inicial do projeto</h1>
+<?php print "impresso em php";?>
 </body>
 </html>
