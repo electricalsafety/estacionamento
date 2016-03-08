1f946f30f1cd864c65e0b77242ba14a8ea7d079b
Alex Sales <electricalsafety@terra.com.br>
2016-03-07 21:54:41 -0300

    Estruturação da página inicial do projeto

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -1,1 +1,10 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Pagina inicial do projeto</title>
+</head>
+<body>
+Pagina inicial do projeto	
+</body>
+</html>
